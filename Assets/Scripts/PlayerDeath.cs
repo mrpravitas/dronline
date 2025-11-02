5bb1fed3e314bca6990f2e78e389b18c0e2a6ef1
artem <homelesspravitas@gmail.com>
2025-11-02 22:00:40 +0600

    Препятствия теперь могут убить игрока

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour, IKillable
+{
+    public void Kill()
+    {
+        Debug.Log("Kill player");
+    }
+}
---
+++ Assets/Scripts/PlayerKiller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour 
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!collider.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (collider.TryGetComponent<IKillable>(out IKillable killable))
+        {
+            killable.Kill();
+        }
+    }
+}
